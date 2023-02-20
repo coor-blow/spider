750173e9db889e282ca20fc498060ece9b063cf6
siwar <sewar.mahmou@gmail.com>
2023-02-20 23:54:14 +0100

    round four

--- game.sh
+++ game.sh
@@ -3,3 +3,4 @@ echo "today we are going to try something new from scratch"
 
 
 echo "now we are doing our round three of edditing the text using vs code and terminal  lets see "
+ echo "round four after some technical mistakes"
