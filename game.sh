b9943ecb600e1659f6aae1f0808243b0afa93b88
siwar <sewar.mahmou@gmail.com>
2023-02-20 23:46:15 +0100

    round three

--- game.sh
+++ game.sh
@@ -2,3 +2,4 @@ echo "hello from me"
 echo "today we are going to try something new from scratch"
 
 
+echo "now we are doing our round three of edditing the text using vs code and terminal  lets see "
